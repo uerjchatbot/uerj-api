2016cc9c6629b833dcaa6983d3afa5221f8dedea
Jhollyfer Rodrigues <jhollyfer.fr@gmail.com>
2023-06-22 22:20:01 -0400

    :ambulance: Remove

--- dist/database/seeders/Questions/egress/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EgressQuestions = void 0;
-const home_1 = require("./home");
-exports.EgressQuestions = [...home_1.HomeQuestions];
-//# sourceMappingURL=index.js.map
--- dist/database/seeders/Questions/master/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MasterQuestions = void 0;
-const home_1 = require("./home");
-exports.MasterQuestions = [...home_1.HomeQuestions];
-//# sourceMappingURL=index.js.map
